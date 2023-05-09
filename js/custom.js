8f0fcb68051aa1fc14d484acb8c19956e7ffe40f
sumitgocinico2023 <sumit@gocinico.com>
2023-05-09 18:51:56 +0530

    Second day

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
-$(".steps li:nth-child(1)").addClass('step--complete');
+//$(".steps li:nth-child(1)").addClass('step--complete');
 
-$('.steps').on('click', '.step--active', function() {
-    $(this).removeClass('step--incomplete').addClass('step--complete');
-    $(this).removeClass('step--active').addClass('step--inactive');
-    $(this).next().removeClass('step--inactive').addClass('step--active');
-  });
+// $('.steps').on('click', '.step--active', function() {
+//     $(this).removeClass('step--incomplete').addClass('step--complete');
+//     $(this).removeClass('step--active').addClass('step--inactive');
+//     $(this).next().removeClass('step--inactive').addClass('step--active');
+//   });
   
-$('.steps').on('click', '.step--complete', function() {
-    $(this).removeClass('step--complete').addClass('step--incomplete');
-    $(this).removeClass('step--inactive').addClass('step--active');
-    $(this).nextAll().removeClass('step--complete').addClass('step--incomplete');
-    $(this).nextAll().removeClass('step--active').addClass('step--inactive');
-});
-  
+// $('.steps').on('click', '.step--complete', function() {
+//     $(this).removeClass('step--complete').addClass('step--incomplete');
+//     $(this).removeClass('step--inactive').addClass('step--active');
+//     $(this).nextAll().removeClass('step--complete').addClass('step--incomplete');
+//     $(this).nextAll().removeClass('step--active').addClass('step--inactive');
+// });
+
 
 var currentMonth = new Date().getMonth();
 var currentYear = new Date().getFullYear();
